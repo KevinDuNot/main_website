3070189335925cf5e5d70b0aa387fb0fea834162
Sunny Chen <sunnychenaustralia@gmail.com>
2026-01-04 20:10:17 +1100

    Added second path effect for hero component

--- src/lib/components/mainpage/Hero.svelte
+++ src/lib/components/mainpage/Hero.svelte
@@ -26,7 +26,7 @@
     }
 </script>
 
-<div class="relative flex w-full md:h-[85vh] h-[90vh] items-center overflow-hidden text-white justify-center">
+<div class="relative flex w-full md:h-[85vh] h-[100vh] items-center overflow-hidden text-white justify-center">
 
     <img
             src={Hero_Background}
@@ -62,8 +62,9 @@
                             style="transform: translateX({-tiltY * 0.5}px) translateY({-tiltX * 0.5}px)"
                     />
 
+<!--                This is the path effect. The two paths are under one svg so you don't have to worry about alignment issues -->
                     <svg
-                            class="absolute z-15 -bottom-[283px] -right-[275px] w-[100%] h-auto pointer-events-none opacity-80"
+                            class="absolute z-15 xl:-bottom-[283px] xl:-right-[275px] lg:-bottom-[230px] lg:-right-[220px] md:-bottom-[230px] md:-right-[200px] sm:-bottom-[190px] sm:-right-[180px] -bottom-[140px] -right-[120px] w-[100%] h-auto pointer-events-none opacity-80"
                             viewBox="0 -50 500 800"
                             style="transform: rotate(5deg) translateX({-tiltY * 0.6}px) translateY({-tiltX * 0.6}px)"
                     >
@@ -75,16 +76,28 @@
                         </defs>
 
                         <path
-                                id="customCurve"
+                                id="binaryCurve"
                                 fill="none"
                                 d="M1.994,150.591 C10.979,-15.143 170.216,-14.039 217.926,20.550 C253.429,46.289 300.280,116.014 197.722,222.556 C163.996,257.591 119.223,280.066 96.701,343.760 C74.233,407.299 10.150,636.256 646.002,719.996"
                         />
-
-                        <text font-family="monospace" font-weight="bold" font-size="15" fill="url(#trailFade)" letter-spacing="1">
-                            <textPath href="#customCurve" startOffset="5%">
+                        <text class="font-mono" font-weight="bold" font-size="15" fill="url(#trailFade)" letter-spacing="1">
+                            <textPath href="#binaryCurve" startOffset="5%">
                                 101110101010101010101010111010101000101010101010010100001010010101010101010101010101010010101010101010101010101001010100101010101010101011010101010101010
                             </textPath>
                         </text>
+
+                        <g transform="translate(-23, 30) scale(0.57)">
+                            <path
+                                    id="mathCurve"
+                                    fill="none"
+                                    d="M1.987,266.019 C54.079,-5.912 311.857,-39.130 379.812,38.430 C462.926,133.291 426.834,222.263 256.080,376.499 C133.410,487.301 135.944,507.466 132.348,621.764 C128.487,744.432 70.726,1139.797 1119.997,1278.014"
+                            />
+                            <text class="font-mono" font-weight="bold" font-size="26" fill="url(#trailFade)" letter-spacing="2">
+                                <textPath href="#mathCurve" startOffset="5%">
+                                    f(x)=x^2∫_a^bf(t)dt=F(b)-F(a)σ_x^2=E[(X-μ)^2]∑_[n=0]^∞(x^n)/n!=e^x∇⋅E=ρ/ε(ε&gt;0,δ&gt;0)|x-c|&lt;δ→|f(x)-L|&lt;εlim_h→0 (f(x+h)-f(x))/h=f'(x)
+                                </textPath>
+                            </text>
+                        </g>
                     </svg>
 
                     <img
