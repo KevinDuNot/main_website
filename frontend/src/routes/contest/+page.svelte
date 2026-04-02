6f4d02bc4e53e1e8f1e60350d79a1fa812e4255a
Oscar Pritchard <oggyp@oggyp.com>
2026-04-03 00:19:44 +1100

    changed contests splash page

--- frontend/src/routes/contest/+page.svelte
+++ frontend/src/routes/contest/+page.svelte
@@ -16,23 +16,52 @@
 </script>
 
 <div class="container">
-
-    <h1 class="text-5xl font-bold text-[#5b8ddb]">
-        IMC Coding Contest
+    <h1 class="text-5xl font-bold text-[#5b8ddb] mb-10 text-center">
+        CPMSoc Contests
     </h1>
 
-    <p class="text-2xl leading-relaxed py-6">
-        Choose your division
-    </p>
-
-    <div class="flex flex-row justify-center px-10 gap-10">
-        <p class="text-3xl leading-relaxed">
-            <a class="underline" href="https://beginner.unswcpmsoc.com">beginner</a>
-        </p>
-
-        <p class="text-3xl leading-relaxed">
-            <a class="underline" href="https://advanced.unswcpmsoc.com">advanced</a>
-        </p>
+    <div class="mb-8 w-full max-w-4xl text-center mt-10">
+        <div class="flex flex-row justify-center px-4 sm:px-10 gap-10">
+            <p class="text-3xl font-bold text-[#5b8ddb] mb-4">
+                <a
+                    class="underline"
+                    href="https://afc26.unswcpmsoc.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    April Fools Contest 2026
+                </a>
+            </p>
+        </div>
     </div>
 
+    <div class="mb-2 w-full max-w-4xl text-center mt-10">
+        <h2 class="text-3xl font-bold text-[#5b8ddb]">
+            IMC Contest (Ended)
+        </h2>
+
+        <p class="text-2xl leading-relaxed py-6">
+            Choose your division
+        </p>
+
+        <div class="flex flex-row justify-center px-4 sm:px-10 gap-10">
+            <p class="text-3xl leading-relaxed">
+                <a
+                    class="underline"
+                    href="https://beginner.unswcpmsoc.com"
+                >
+                    beginner
+                </a>
+            </p>
+
+            <p class="text-3xl leading-relaxed">
+                <a
+                    class="underline"
+                    href="https://advanced.unswcpmsoc.com"
+                >
+                    advanced
+                </a>
+            </p>
+        </div>
+    </div>
 </div>
